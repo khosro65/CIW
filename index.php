0101a9227e68ab834f4653718d489f1623c74064
khosro65 <khosro65@github.com>
2019-12-22 17:40:05 +0330

    merge commit

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ $myname = "Khosro";
 echo "Hello World my name is";
 echo "</br>";
 echo $myname;
+
+$age = 25;
+function birthday()
+{
+    $GLOBALS['age']++;
+}
+
+birthday();
+echo  "Happy birh day Khosro:".$age;
 ?>
 </body>
 </html>
